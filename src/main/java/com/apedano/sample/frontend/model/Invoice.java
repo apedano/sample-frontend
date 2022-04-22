429cc9146a33a10ebb22e20d75ed44396949ba3e
Alessandro Pedano <pedano.alessandro@gmail.com>
2022-04-22 13:41:03 +0200

    Added h2 support. Added Tymeleaf support Added templates in the right place Changed dependency for webapp impl Changed Id generation strategy supported by H2

--- src/main/java/com/apedano/sample/frontend/model/Invoice.java
+++ src/main/java/com/apedano/sample/frontend/model/Invoice.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 public class Invoice {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String location;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO INVOICE values (1, 10.5, 'Location1', 'Name1');
